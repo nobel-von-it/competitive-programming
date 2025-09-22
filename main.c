c0e639cabbf5023739e7275ff9b2a9fae5d79111
nobel-von-it <maksimdavydenko12@gmail.com>
2025-09-23 00:22:36 +0700

    edit: l2396 (easy)

--- main.c
+++ main.c
@@ -4,28 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-bool isStrictlyPalindromic(int n)
-{
-    for (int b = 2; b < n - 2; b++) {
-        char buf[256];
-        int tmp = n;
-        int in = 1;
-        int ib = 0;
-        while (tmp > 0) {
-            buf[ib++] = (tmp % b) + '0';
-            in *= 10;
-            tmp /= b;
-        }
-        for (int i = 0; i < ib / 2; i++) {
-            printf("cmp bi %c and bib %c\n", buf[i], buf[ib - i]);
-            if (buf[i] != buf[ib - i - 1]) return false;
-        }
-        buf[ib++] = '\0';
-        printf("%d base %d is %s\n", n, b, buf);
-        memset(buf, 0, sizeof(buf));
-    }
-    return true;
-}
+bool isStrictlyPalindromic(int n) { return false; }
 
 int main(void)
 {
