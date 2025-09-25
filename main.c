54f57aafe9f5b5e6fc7bcaeb90f328075f11590b
nobel-von-it <maksimdavydenko12@gmail.com>
2025-09-25 23:51:25 +0700

    done: l771

--- main.c
+++ main.c
@@ -4,17 +4,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-int *recoverOrder(int *order, int orderSize, int *friends, int friendsSize, int *returnSize)
+int numJewelsInStones(char *jewels, char *stones)
 {
-    int *res = malloc(sizeof(int) * friendsSize);
-    int ri = 0;
-    for (int i = 0; i < orderSize; i++) {
-        for (int j = 0; j < friendsSize; j++) {
-            if (order[i] == friends[j]) res[ri++] = order[i];
+    int res = 0, jl = strlen(jewels), sl = strlen(stones);
+    for (int i = 0; i < jl; i++) {
+        for (int j = 0; j < sl; j++) {
+            if (jewels[i] == stones[j]) res++;
         }
     }
-
-    *returnSize = friendsSize;
     return res;
 }
 
